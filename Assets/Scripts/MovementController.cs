090c5303da5cc18abf07a65ceb10330ae192bb52
sergigb <srgb7879@gmail.com>
2024-02-06 18:07:54 +0100

    ship movement with the keys

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+    public float movementSpeed;
+    public float rotation;
+
+    // Start is called before the first frame update
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        rotation = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Make this look at the ship's whele
+        rb.rotation += Input.GetAxis(horizontalAxis)*-5;
+
+        Vector2 forward = new Vector2(Mathf.Cos(rb.rotation * Mathf.Deg2Rad), Mathf.Sin(rb.rotation * Mathf.Deg2Rad));
+        rb.velocity = forward * movementSpeed * Input.GetAxis(verticalAxis);  // and this the product between the sail (vela?) and the wind
     }
 }
